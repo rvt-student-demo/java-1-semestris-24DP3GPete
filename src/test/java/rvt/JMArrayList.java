f23d1d4a829978e17514366a24f05901430750d4
24DP3GPete <a240762gp@rvt.lv>
2025-10-30 13:03:54 +0200

    JMArrayList

---
+++ src/test/java/rvt/JMArrayList.java
@@ -0,0 +1,68 @@
+package rvt;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class JMArrayList {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void onlyTheseNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i != -1; i = Integer.parseInt(scanner.nextLine())) {
+            numbers.add(i);
+        }
+
+        System.out.print("From where? ");
+        int from = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("To where? ");
+        int to = Integer.parseInt(scanner.nextLine());
+
+        for (int i = from; i == to+1; i++) {
+            System.out.println(numbers.get(i));
+        }
+    }
+
+    public static void listSize() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
+
+        for (String i = "0"; i != ""; i = scanner.nextLine()) {
+            names.add(i);
+        }
+        System.out.println(names.size());
+    }
+
+    public static void onTheList() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
+
+        for (String i = "0"; i != ""; i = scanner.nextLine()) {
+            names.add(i);
+        }
+        System.out.print("Search for? ");
+        String target = scanner.nextLine();
+        if (names.contains(target)) {
+            System.out.println(target + "was found!");
+        } else {
+            System.out.println(target + "was not found!");
+        }
+    }
+
+    public static void removeLast(ArrayList<String> strings) {
+
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+
+        strings.remove(strings.size()-1);
+        strings.remove(strings.size()-1);
+
+        System.out.println(strings);
+    }
+}
